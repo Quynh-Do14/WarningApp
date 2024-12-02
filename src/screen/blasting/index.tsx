fde789c3c4a2f4bf2edeeb229151eae355590235
Quynh Do <doducquynh14022000@gmail.com>
2024-12-02 20:44:18 +0700

    first commit

---
+++ src/screen/blasting/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const BlastingScreen = () => {
+    return (
+        <View>
+            <Text>BlastingScreen</Text>
+        </View>
+    )
+}
+
+export default BlastingScreen
+
+const styles = StyleSheet.create({})
